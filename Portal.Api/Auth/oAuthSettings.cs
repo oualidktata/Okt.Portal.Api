8e9a10e7b4521a1752d6d5fa96ba9836658971b5
Oualid Ktata <oualid.ktata@gmail.com>
2019-08-22 09:46:57 -0400

    Update oAuthSettings.cs

--- Portal.Api/Auth/oAuthSettings.cs
+++ Portal.Api/Auth/oAuthSettings.cs
@@ -10,16 +10,16 @@ namespace Portal.Api.Auth
         //public static string AuthorizationUrl  => "/api/oauth/auth";
         //public static string CallBackUrl => "/api/oauth/callback";
 
-        public static string ClientId => "0oauaatqnDFhZFgkp356";
-        public static string ClientSecret => "wHWb6jbjEXP21Y0pJ6JvuYejBDmLL13CE4qHSkV8";
+        public static string ClientId => "SECRET";
+        public static string ClientSecret => "SECRET";
 
-        public static string BasicAuthBase64 => "Basic MG9hdWFhdHFuREZoWkZna3AzNTY6d0hXYjZqYmpFWFAyMVkwcEo2SnZ1WWVqQkRtTEwxM0NFNHFIU2tWOA==";
+        public static string BasicAuthBase64 => "Basic SECRET==";
 
         public static string AuthHeaderName => "Authorization";
         public static string ApiBaseUri => "https://localhost:44324/";
-        public static string IssuerURI => "https://dev-193568.okta.com/oauth2/default";
+        public static string IssuerURI => "https://dev-SECRET.okta.com/oauth2/default";
         public static string[] Audiences => new[] { "api://default" };
-        public static string OktaTokenUrl => "https://dev-193568.okta.com/oauth2/default/v1/token";
+        public static string OktaTokenUrl => "https://dev-SECRET.okta.com/oauth2/default/v1/token";
 
         public static IDictionary<string, string> Scopes => new Dictionary<string, string>() {
                                     { "custom_scope", "custom scope for CC defined in OKTA" },
